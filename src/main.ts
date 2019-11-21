4ab6a7397af75457884ad2736d270fe1878e7748
Peter Bjorklund <piot@hotmail.com>
2019-11-21 22:27:26 +0100

    fix: use x-oauth-basic as password

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,12 @@ async function setPrivate(): Promise<number> {
   let repo = core.getInput("repo")
   core.info(`Setting up Go private modules for repo prefix '${repo}'`);
 
-  const username = core.getInput("username")
-  const token = core.getInput("token")
+  const username = core.getInput("token")
+  const password = "x-oauth-basic"
 
-  const content = `machine github.com login ${username} password ${token}`
+  const content = `machine github.com login ${username} password ${password}`
   const netrcFile = path.join(process.env.HOME as string, ".netrc");
-  core.debug(`writing ${netrcFile} with ${content}`)
+  core.info(`writing ${netrcFile} with ${content}`)
   fs.writeFileSync(netrcFile, content)
   
   return exec.exec(`go env -w GOPRIVATE="${repo}"`)
