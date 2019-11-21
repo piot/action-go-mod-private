701fb8ee7bf6ec1aa8dbb28934d6b5ef299059f7
Peter Bjorklund <piot@hotmail.com>
2019-11-21 22:31:21 +0100

    fix: back to username and token

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ async function setPrivate(): Promise<number> {
   let repo = core.getInput("repo")
   core.info(`Setting up Go private modules for repo prefix '${repo}'`);
 
-  const username = core.getInput("token")
-  const password = "x-oauth-basic"
+  const username = core.getInput("username")
+  const password = core.getInput("token")
 
   const content = `machine github.com login ${username} password ${password}`
   const netrcFile = path.join(process.env.HOME as string, ".netrc");
